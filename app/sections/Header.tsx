ff8c2cf652eb2b64a1ec85ac614ebb2a0693639f
meetkamal256 <meetkamal256@gmail.com>
2025-06-04 01:41:18 +0100

    feat(header): build responsive header section for mobile, tablet, and desktop

--- app/sections/Header.tsx
+++ app/sections/Header.tsx
@@ -2,6 +2,43 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header> Navbar</header>
+    <header className="sticky top-0">
+      <div className="flex justify-center items-center py-4 bg-black text-white">
+        <div className="flex gap-3 items-center justify-center">
+          <p className="text-white/60 text-sm">
+            Streamline Your workflow and boost your productivity
+          </p>
+          <p>Get started for free</p>
+        </div>
+        <img
+          src="/assets/arrow-right.svg"
+          alt="Arrow"
+          className="h-4 w-4 ml-2 inline-flex justify-center items-center"
+        />
+      </div>
+
+      <div className="container py-5">
+        <div className="flex justify-between items-center">
+          <Image src="/assets/logosaas.png" alt="" height={40} width={40} />
+          <Image
+            src="/assets/menu.svg"
+            alt="menuIcon"
+            width={20}
+            height={20}
+            className="md:hidden"
+          />
+          <nav className="hidden md:flex gap-6 text-black/60 items-center">
+            <a href="#">About</a>
+            <a href="#">Features</a>
+            <a href="#">Customers</a>
+            <a href="#">Updates</a>
+            <a href="#">Help</a>
+            <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex items-center justify-center tracking-tight">
+              Get for free
+            </button>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 };
