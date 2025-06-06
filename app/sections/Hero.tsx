2cf8334609cd8172e0310102cb22aa9d9dc5916f
meetkamal256 <meetkamal256@gmail.com>
2025-06-06 17:58:26 +0100

    feat(hero): add Hero section structure with partial styling

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -1,7 +1,24 @@
 export const Hero = () => {
   return (
     <section>
-      <div>Hero</div>
+      <div className="container">
+        <div>
+          <div className="text-sm inline-flex border border-[#222]/10 py-1 px-3 rounded-lg tracking-tight">
+            Version 2.0 is here
+          </div>
+          <h1 className="font-bold text-5xl bg-gradient-to-b from-black to-[#001E7F] text-transparent bg-clip-text mt-6">
+            Pathway to productivity
+          </h1>
+          <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts, and celebrate your successes.
+          </p>
+          <div className="flex gap-1 items-center mt-[30px]">
+            <button>Get for free</button>
+            <button>Learn more</button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
