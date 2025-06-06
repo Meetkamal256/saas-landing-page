765ddf3b1f44e347cb1c3d1b4f7d1d9e368b242c
meetkamal256 <meetkamal256@gmail.com>
2025-06-06 21:57:25 +0100

    feat: build and style hero section for mobile, tablet, and desktop

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -1,26 +1,54 @@
 import { LuArrowRight } from "react-icons/lu";
+import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <section>
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_50%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
       <div className="container">
-        <div>
-          <div className="text-sm inline-flex border border-[#222]/10 py-1 px-3 rounded-lg tracking-tight">
-            Version 2.0 is here
+        <div className="md:flex items-center">
+          <div className="md:w-[478px] lg:w-1/2">
+            <div className="text-sm inline-flex border border-[#222]/10 py-1 px-3 rounded-lg tracking-tight">
+              Version 2.0 is here
+            </div>
+            <h1 className="font-bold text-5xl md:text-7xl bg-gradient-to-b from-black to-[#001E7F] text-transparent bg-clip-text mt-6">
+              Pathway to productivity
+            </h1>
+            <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+              Celebrate the joy of accomplishment with an app designed to track
+              your progress, motivate your efforts, and celebrate your
+              successes.
+            </p>
+            <div className="flex gap-1 items-center mt-[30px]">
+              <button className="btn btn-primary">Get for free</button>
+              <button className="btn btn-text">
+                <span>Learn more</span>
+                <LuArrowRight className="h-5 w-5 ml-1" />
+              </button>
+            </div>
           </div>
-          <h1 className="font-bold text-5xl bg-gradient-to-b from-black to-[#001E7F] text-transparent bg-clip-text mt-6">
-            Pathway to productivity
-          </h1>
-          <p className="text-xl text-[#010D3E] tracking-tight mt-6">
-            Celebrate the joy of accomplishment with an app designed to track
-            your progress, motivate your efforts, and celebrate your successes.
-          </p>
-          <div className="flex gap-1 items-center mt-[30px]">
-            <button className="btn btn-primary">Get for free</button>
-            <button className="btn btn-text">
-              <span>Learn more</span>
-              <LuArrowRight className="h-5 w-5 ml-1" />
-            </button>
+          
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
+            <Image
+              src="/assets/cog.png"
+              width={648}
+              height={648}
+              alt="cog image"
+              className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+            />
+            <Image
+              src="/assets/cylinder.png"
+              width={220}
+              height={220}
+              alt="cylinder image"
+              className="hidden md:block md:absolute md:-top-8 md:-left-32"
+            />
+            <Image
+              src="/assets/noodle.png"
+              width={220}
+              height={220}
+              alt="noodle image"
+              className="hidden lg:block lg:absolute top-[524px] left-[448px] rotate-[30deg]"
+            />
           </div>
         </div>
       </div>
