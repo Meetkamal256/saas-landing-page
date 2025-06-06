67ea8465dbef944a500512c994dfc99f04d28190
meetkamal256 <meetkamal256@gmail.com>
2025-06-06 18:12:30 +0100

    feat(styles): add reusable button styles  with .btn,  .btn-primary and .btn-text using tailwind component layer

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -1,3 +1,5 @@
+import { LuArrowRight } from "react-icons/lu";
+
 export const Hero = () => {
   return (
     <section>
@@ -14,8 +16,11 @@ export const Hero = () => {
             your progress, motivate your efforts, and celebrate your successes.
           </p>
           <div className="flex gap-1 items-center mt-[30px]">
-            <button>Get for free</button>
-            <button>Learn more</button>
+            <button className="btn btn-primary">Get for free</button>
+            <button className="btn btn-text">
+              <span>Learn more</span>
+              <LuArrowRight className="h-5 w-5 ml-1" />
+            </button>
           </div>
         </div>
       </div>
