426f9a1a9ca6ccea56ef6c630fb0dec129f20432
meetkamal256 <meetkamal256@gmail.com>
2025-06-07 21:42:09 +0100

    feat:(styles): add reusable styles .sectionTitle, sectionDescription using tailwind component layer  and scaffold basic pricing page with minimal content

--- app/sections/Pricing.tsx
+++ app/sections/Pricing.tsx
@@ -1,3 +1,17 @@
+import { pricingTiers } from "../data";
+
 export const Pricing = () => {
-  return null;
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text ">
+          Pricing
+        </h2>
+        <p className="section-description mt-5">
+          Free forever. Upgrade for unlimited tasks, better security, and
+          exclusive features.
+        </p>
+      </div>
+    </section>
+  );
 };
