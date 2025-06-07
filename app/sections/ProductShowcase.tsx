24094b89da1d9361ef74c08f1327edd4c0552bb1
meetkamal256 <meetkamal256@gmail.com>
2025-06-07 01:44:20 +0100

    feat: scaffold ProductShowcase section with content, image, and gradient background

--- app/sections/ProductShowcase.tsx
+++ app/sections/ProductShowcase.tsx
@@ -1,3 +1,25 @@
+import Image from "next/image";
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-[#ffff] to-[#D2DCFF]">
+      <div className="container">
+        <div>Boost your productivity</div>
+        <h2>A more effective way to track progress</h2>
+        <p>
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress and motivate your efforts.
+        </p>
+        <div className="relative w-full aspect-[16/9]">
+          <Image
+            src="/assets/product-image.png"
+            alt="product Image"
+            fill
+            className="object-contain"
+            priority
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
