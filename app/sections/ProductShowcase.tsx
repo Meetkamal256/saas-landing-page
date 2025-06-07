fa023c14e0377b7f3554f7f1b58eb11b686b4c91
meetkamal256 <meetkamal256@gmail.com>
2025-06-07 02:25:03 +0100

    feat: build productShowCase section for mobile

--- app/sections/ProductShowcase.tsx
+++ app/sections/ProductShowcase.tsx
@@ -4,13 +4,15 @@ export const ProductShowcase = () => {
   return (
     <section className="bg-gradient-to-b from-[#ffff] to-[#D2DCFF]">
       <div className="container">
-        <div className="tag">Boost your productivity</div>
-        <h2>A more effective way to track progress</h2>
-        <p>
+        <div className="flex justify-center">
+          <div className="tag">Boost your productivity</div>
+        </div>
+        <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">A more effective way to track progress</h2>
+        <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
           Celebrate the joy of accomplishment with an app designed to track your
           progress and motivate your efforts.
         </p>
-        <div className="relative w-full aspect-[16/9]">
+        <div className="relative w-full aspect-[16/9] mt-5">
           <Image
             src="/assets/product-image.png"
             alt="product Image"
