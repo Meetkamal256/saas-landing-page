0929b7dd1bb5c49efe766c8414e73117af7f2310
meetkamal256 <meetkamal256@gmail.com>
2025-06-08 14:26:13 +0100

    feat: build testimonial section for mobile

--- app/sections/Testimonials.tsx
+++ app/sections/Testimonials.tsx
@@ -20,20 +20,20 @@ export const Testimonials = () => {
           </p>
         </div>
         {/* testimonial card */}
-        <div>
+        <div className="flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]">
           {firstColumn.map((testimonial) => (
             <div
               key={testimonial.id}
               className="card shadow[0_7px_14px_#EAEAEA]"
             >
-              <div>{testimonial.text}</div>
-              <div>
+              <div className="">{testimonial.text}</div>
+              <div className="flex items-center gap-2 mt-5">
                 <div>
-                  <Image src={testimonial.imageSrc} alt="testimonial image" height={40} width={40}/>
+                  <Image src={testimonial.imageSrc} alt="testimonial image" height={40} width={40} className="full-rounded h-10 w-10"/>
                 </div>
-                <div>
-                  <div>{testimonial.name}</div>
-                  <div>{testimonial.username}</div>
+                <div className="flex flex-col">
+                  <div className="font-medium tracking-tight leading-[20px]">{testimonial.name}</div>
+                  <div className="leading-5 tracking-tight ">{testimonial.username}</div>
                 </div>
               </div>
             </div>
