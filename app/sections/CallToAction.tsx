a8b1454a031e128b770aff7e0644efa634c1f4d5
meetkamal256 <meetkamal256@gmail.com>
2025-06-09 10:34:30 +0100

    feat: build CTA section for tablet and desktop

--- app/sections/CallToAction.tsx
+++ app/sections/CallToAction.tsx
@@ -1,15 +1,29 @@
 import { FaArrowRightLong } from "react-icons/fa6";
-
+import Image from "next/image";
 export const CallToAction = () => {
   return (
-    <section className="bg-white py-24">
+    <section className="bg-white py-24 overflow-x-clip">
       <div className="container">
-        <div className="section-heading">
+        <div className="section-heading relative">
           <h2 className="section-title">Sign up for today</h2>
           <p className="section-description mt-5">
             Celebrate the joy of accomplishment with an app designed to track
             your progress and motivate your efforts.
           </p>
+          <Image
+            src="/assets/star.png"
+            alt="star image"
+            width={360}
+            height={360}
+            className="hidden md:block absolute -left-[350px] -top-[137px]"
+          />
+          <Image
+            src="/assets/spring.png"
+            alt="spring image"
+            width={360}
+            height={360}
+            className="hidden md:block absolute -right-[331px] -top-[19px]"
+          />
         </div>
 
         <div className="flex justify-center gap-4 mt-5">
