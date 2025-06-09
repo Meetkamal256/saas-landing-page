a18333d7352121e450c50ad56278d8c02054e6c2
meetkamal256 <meetkamal256@gmail.com>
2025-06-09 09:24:25 +0100

    feat: build CTA section for mobile

--- app/sections/CallToAction.tsx
+++ app/sections/CallToAction.tsx
@@ -1,3 +1,25 @@
+import { FaArrowRightLong } from "react-icons/fa6";
+
 export const CallToAction = () => {
-  return null;
+  return (
+    <section className="bg-white py-24">
+      <div className="container">
+        <div className="section-heading">
+          <h2 className="section-title">Sign up for today</h2>
+          <p className="section-description mt-5">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and motivate your efforts.
+          </p>
+        </div>
+
+        <div className="flex justify-center gap-4 mt-5">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text">
+            <span>Learn more</span>
+            <FaArrowRightLong className="ml-1" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
