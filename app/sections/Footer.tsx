8362eb4f182cda24227cb2b3075b569785184b9f
meetkamal256 <meetkamal256@gmail.com>
2025-06-09 10:53:24 +0100

    feat: add footer section with JSX structure and content

--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -1,3 +1,39 @@
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaPinterestP } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+
+import Image from "next/image";
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer>
+      <div className="container">
+        <div>
+          <Image
+            src="/assets/logosaas.png"
+            alt="logo image"
+            width={40}
+            height={40}
+          />
+        </div>
+        <nav>
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+        </nav>
+        <div>
+          <FaXTwitter />
+          <FaInstagram />
+          <FaLinkedinIn />
+          <FaPinterestP />
+          <FaYoutube />
+        </div>
+        <div>© 2024 Your Company, Inc. All rights reserved.</div>
+      </div>
+    </footer>
+  );
 };
